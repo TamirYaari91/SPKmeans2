88f2c3770359e936b0af4887adb52ad1e3ed520e
TamirYaariTAU <73110585+TamirYaariTAU@users.noreply.github.com>
2021-08-24 19:50:13 +0300

    Add files via upload

--- spkmeansmodule.c
+++ spkmeansmodule.c
@@ -1,42 +1,49 @@
-#define PY_SSIZE_T_CLEAN  /* For all # variants of unit formats (s#, y#, etc.) use Py_ssize_t rather than int. */
+#define PY_SSIZE_T_CLEAN
+
 #include <Python.h>
-/*
-#include <python3.7/Python.h>
-*/
-
-             /* MUST include <Python.h>, this implies inclusion of the following standard headers:
-                             <stdio.h>, <string.h>, <errno.h>, <limits.h>, <assert.h> and <stdlib.h> (if available). */
-#include <math.h>         /* include <Python.h> has to be before any standard headers are included */
+#include <math.h>
 #include "spkmeans.h"
-
 /*
 #include "kmeans.h"
 */
-
 #include "kmeans2.h"
 
-/*
-#include <Python.h>
-*/
-/*
- * Helper function that will not be exposed (meaning, should be static)
- */
+#define FUNC(_flag, _name, _docstring) { #_name, (PyCFunction)_name, _flag, PyDoc_STR(_docstring) }
 
-/*
- * A geometric series up to n. sum_up_to_n(z^n)
- */
 
 static PyObject *mat_to_Python_mat(double **mat, int, int); /*not sure if needs to be static or not*/
 
-static PyObject * kmeans2_py(int, int, int, PyObject *, PyObject *, int, int);
+static PyObject *kmeans2_py(int, int, int, PyObject *, PyObject *, int, int);
 
 static PyObject *spkmeans_Python(char *, char *, int, int);
 
 static PyObject *kmeans2(int, int, int, PyObject *,
                          PyObject *, int, int);
 
+static PyObject *fit(PyObject *, PyObject *);
 
-static PyObject *spkmeans_Python(char *filename, char *goal, int k, int source) { /*source == 0 -> C | source == 1 -> Python*/
+static PyObject *fit2(PyObject *, PyObject *);
+
+
+static PyMethodDef capiMethods[] = {
+        FUNC(METH_VARARGS, fit, "first part"),
+        FUNC(METH_VARARGS, fit2, "second part"),
+        {NULL, NULL, 0, NULL}
+};
+
+
+static struct PyModuleDef moduleDef = {
+        PyModuleDef_HEAD_INIT, "myspkmeans", NULL, -1, capiMethods};
+
+
+PyMODINIT_FUNC
+PyInit_myspkmeans(void) {
+    return PyModule_Create(&moduleDef);
+}
+
+
+static PyObject *
+spkmeans_Python(char *filename, char *goal, int k, int source) { /*source == 0 -> C | source == 1 -> Python*/
     int i, N, dim;
     int *N_dim;
     double *eigenvalues;
@@ -124,7 +131,7 @@ static PyObject *spkmeans_Python(char *filename, char *goal, int k, int source) 
             item.vector = eigenvector;
             eigen_items[i] = item;
         }
-        mergeSort(eigen_items,0,N-1);
+        mergeSort(eigen_items, 0, N - 1);
         if (k == 0) {
             k = eigen_gap(eigen_items, N);
         }
@@ -213,8 +220,8 @@ static PyObject *kmeans2(int k, int num_of_lines, int dim, PyObject *centroids_p
 }
 
 
-static PyObject * kmeans2_py(int k, int num_of_lines, int dim, PyObject *centroids_py,
-                      PyObject *points_to_cluster_py, int centroids_length, int points_to_cluster_length) {
+static PyObject *kmeans2_py(int k, int num_of_lines, int dim, PyObject *centroids_py,
+                            PyObject *points_to_cluster_py, int centroids_length, int points_to_cluster_length) {
     return kmeans2(k, num_of_lines, dim, centroids_py,
                    points_to_cluster_py, centroids_length, points_to_cluster_length);
 };
@@ -257,7 +264,7 @@ static PyObject *fit(PyObject *self, PyObject *args) {
 /* This builds the answer ("d" = Convert a C double to a Python floating point number) back into a python object */
     /*return Py_BuildValue("O",
                          spkmeans(filename, goal, k, 1));*/ /*  Py_BuildValue(...) returns a PyObject*  */
-            return Py_BuildValue("O",spkmeans_Python(filename, goal, k, 1));
+    return Py_BuildValue("O", spkmeans_Python(filename, goal, k, 1));
 }
 
 static PyObject *fit2(PyObject *self, PyObject *args) {
@@ -276,48 +283,12 @@ static PyObject *fit2(PyObject *self, PyObject *args) {
     }
 
     return Py_BuildValue("O", kmeans2_py(k, num_of_lines, dim, centroids_py, points_to_cluster_py,
-                                      centroids_length, points_to_cluster_length));
+                                         centroids_length, points_to_cluster_length));
 }
 
 /*
  * This array tells Python what methods this module has.
  * We will use it in the next structure
  */
-static PyMethodDef capiMethods[] = {
-        {"fit",                   /* the Python method name that will be used */
-         (PyCFunction) fit, /* the C-function that implements the Python function and returns static PyObject*  */
-         METH_VARARGS,           /* flags indicating parametersaccepted for this function */
-         PyDoc_STR("A C function to generate T matrix as per steps 1-6 in Normalized Spectral Clustering Algorithm or generate relevant matrix as per goal")},
-         {"fit2",                   /* the Python method name that will be used */
-         (PyCFunction) fit2, /* the C-function that implements the Python function and returns static PyObject*  */
-         METH_VARARGS,           /* flags indicating parametersaccepted for this function */
-         PyDoc_STR("A C function to perform KMeans calculation after KMeans++ was performed")},
-          /*  The docstring for the function */
-        { NULL, NULL, 0, NULL }     /* The last entry must be all NULL as shown to act as a
-                                 sentinel. Python looks for this entry to know that all
-                                 of the functions for the module have been defined. */
-};
-
-
-static struct PyModuleDef moduleDef = {
-        PyModuleDef_HEAD_INIT,
-        "myspkmeans",
-        NULL,
-        -1,
-        capiMethods
-};
-
-
-/*
- * The PyModuleDef structure, in turn, must be passed to the interpreter in the module’s initialization function.
- * The initialization function must be named PyInit_name(), where name is the name of the module and should match
- * what we wrote in struct PyModuleDef.
- * This should be the only non-static item defined in the module file
- */
-
-PyMODINIT_FUNC
-PyInit_myspkmeans(void) {
-    return PyModule_Create(&moduleDef);
-}
 
 
